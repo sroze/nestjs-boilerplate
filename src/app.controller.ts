988b0d7d8da38e680d0941666547604522eac838
Samuel ROZE <samuel.roze@gmail.com>
2020-03-22 18:15:44 +0000

    Add more business patterns

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import * as crypto from 'crypto';
+import { v4 } from 'uuid';
+
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +15,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/wait/:time')
+  async wait(@Param('time') time: number) {
+    await new Promise(resolve => {
+      setTimeout(resolve, time);
+    });
+
+    return `Waited ${time} milli-seconds.`;
+  }
+
+  @Get('/sleep/:time')
+  async sleep(@Param('time') time: number) {
+    msleep(time);
+
+    return `Slept ${time} milli-seconds.`;
+  }
+
+  @Get('/busy/:time')
+  async busy(@Param('time') time: string) {
+    const stopAt = Date.now() + parseInt(time);
+
+    while (Date.now() <= stopAt) {
+      crypto.createHmac('sha256', v4()).update(v4()).digest('hex');
+    }
+
+    return `Was busy for ${time}`;
   }
 }
